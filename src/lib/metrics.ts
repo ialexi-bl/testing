047109186ea52227cd27f306591fb1b5ac75cd99
Alex <ialexi.bl@gmail.com>
2023-09-09 01:34:32 +0300

    Added tailwind + metrics json schema prototype

---
+++ src/lib/metrics.ts
@@ -0,0 +1,43 @@
+export type MetricsJson = {
+	metrics: Metrics
+}
+
+export type Metrics = Metric[]
+
+export type Metric = LinearMetric | PieMetric | ColumnMetric | NumberMetric
+
+// Конкретные метрики
+
+export enum MetricType {
+	linear = 'linear',
+	pie = 'pie',
+	column = 'column',
+	number = 'number',
+}
+
+type MetricBase = {
+	id: string
+	displayName: string
+}
+
+type LinearMetric = MetricBase & {
+	type: MetricType.linear
+	xUnits?: 'none' | 'datetime'
+
+	data: [x: number, y: number][]
+}
+
+type PieMetric = MetricBase & {
+	type: MetricType.pie
+	data: Record<string, number>
+}
+
+type ColumnMetric = MetricBase & {
+	type: MetricType.linear
+	data: [category: number | string, value: number][]
+}
+
+type NumberMetric = MetricBase & {
+	type: MetricType.number
+	data: number
+}
