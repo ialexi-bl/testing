8172cdcba4f5999ce8c6fa38d68baae6a93f5af1
Alex <ialexi.bl@gmail.com>
2023-09-14 12:35:01 +0300

    Fixed formatting

--- src/lib/metrics.ts
+++ src/lib/metrics.ts
@@ -1,5 +1,5 @@
 export type MetricsJson = {
-	metrics: Metrics
+    metrics: Metrics
 }
 
 export type Metrics = Metric[]
@@ -9,35 +9,35 @@ export type Metric = LinearMetric | PieMetric | ColumnMetric | NumberMetric
 // Конкретные метрики
 
 export const enum MetricType {
-	Linear = 'linear',
-	Pie = 'pie',
-	Column = 'column',
-	Number = 'number',
+    Linear = 'linear',
+    Pie = 'pie',
+    Column = 'column',
+    Number = 'number',
 }
 
 type MetricBase = {
-	id: string
-	displayName: string
+    id: string
+    displayName: string
 }
 
 type LinearMetric = MetricBase & {
-	type: MetricType.Linear
-	xUnits?: 'none' | 'datetime'
+    type: MetricType.Linear
+    xUnits?: 'none' | 'datetime'
 
-	data: [x: number, y: number][]
+    data: [x: number, y: number][]
 }
 
 type PieMetric = MetricBase & {
-	type: MetricType.Pie
-	data: Record<string, number>
+    type: MetricType.Pie
+    data: Record<string, number>
 }
 
 type ColumnMetric = MetricBase & {
-	type: MetricType.Column
-	data: [category: number | string, value: number][]
+    type: MetricType.Column
+    data: [category: number | string, value: number][]
 }
 
 type NumberMetric = MetricBase & {
-	type: MetricType.Number
-	data: number
+    type: MetricType.Number
+    data: number
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import '../app.css'
+    import '../app.css'
 </script>
 
 <div class="flex flex-col h-screen">
-	<nav
-		class="bg-primary sticky top-0 left-0 w-full h-12 flex justify-between items-center border-b border-white/20"
-	>
-		<div class="px-3 font-bold">A/B тестирование</div>
-		<div class="px-3 font-bold">Контакты</div>
-	</nav>
-	<div class="w-full flex-grow">
-		<slot />
-	</div>
+    <nav
+        class="bg-primary sticky top-0 left-0 w-full h-12 flex justify-between items-center border-b border-white/20"
+    >
+        <div class="px-3 font-bold">A/B тестирование</div>
+        <div class="px-3 font-bold">Контакты</div>
+    </nav>
+    <div class="w-full flex-grow">
+        <slot />
+    </div>
 </div>
