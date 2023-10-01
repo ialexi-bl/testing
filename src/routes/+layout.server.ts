2b92e98c39fa443ac71f62ed098d36a55929e186
Коротченок Остап Андреевич <71458498+astatochek@users.noreply.github.com>
2023-10-01 18:46:40 +0300

    Update src/routes/+layout.server.ts
    Co-authored-by: ialexi-bl <44489753+ialexi-bl@users.noreply.github.com>

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,9 +2,9 @@ import type {LayoutServerLoad} from './$types'
 import {getProjects} from '$lib/server/database'
 
 export const load: LayoutServerLoad = ({cookies}) => {
-    const cachedUserID = cookies.get('userID')
-    const userID = cachedUserID ?? crypto.randomUUID()
-    if (userID !== cachedUserID) {
+    let userID = cookies.get('userID')
+    if (!userID) {
+        userID = crypto.randomUUID()
         cookies.set('userID', userID)
     }
     const projects = getProjects(userID)
