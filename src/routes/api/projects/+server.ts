6741be3a07741a3375512a81ab48b9a4a816bdd8
Alex <ialexi.bl@gmail.com>
2023-10-08 21:35:28 +0300

    Eslint fix

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -1,7 +1,8 @@
 import type {RequestHandler} from './$types'
 import {error, json} from '@sveltejs/kit'
 import {getProjects} from '$lib/server/database'
-export const GET: RequestHandler = async ({cookies}) => {
+
+export const GET: RequestHandler = ({cookies}) => {
     const uuid = cookies.get('userID')
     if (uuid === undefined)
         throw error(401, {
